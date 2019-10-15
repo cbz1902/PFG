2809df10a7f4707785b8e95b13de1fddc1a3808c
cbz1902 <carlitosbenitez0095@gmail.com>
2019-10-15 20:28:42 -0300

    Proyecto Final de Grado

--- capturador.py
+++ capturador.py
@@ -1,13 +1,36 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-import Lib.GetOldTweets.got3 as g
+
+import GetOldTweets3 as g
+import  re
 
 def capturar_old_tweets():
     tweetsCriteria = g.manager.TweetCriteria()
-    tweetsCriteria.setQuerySearch("@Asuncionmuni OR @Ferreiromario1")
+    tweetsCriteria.setQuerySearch("@AsuncionMuni OR @ferreiromario1 OR @msaludpy")
     tweetsCriteria.setSince("2017-09-01")
-    tweetsCriteria.setUntil("2018-02-28")
-    tweetsCriteria.setMaxTweets(100)
+    tweetsCriteria.setUntil("2018-03-30")
+    tweetsCriteria.setMaxTweets(200)
     tweet_history = g.manager.TweetManager.getTweets(tweetsCriteria)
-    print(tweet_history)
+    filtrar_tweets(tweet_history)
+
+def buscar(patrones, texto):
+    list = []
+    for patron in patrones:
+        print(re.match(patron, texto))
+    return list
+
+def filtrar_tweets(tweets):
+
+    exp_agua = ['lluvia*','inundacion*','crecida*','agua*','servida*','desag*','llueve','raudal*']
+    exp_basura = ['basura*','recicla*','desecho','toxico','vertedero*','escombro*']
+    exp_dengue = ['dengue','mosquito','aedes','criadero*','minga*','fumiga*']
+    exp_proteccion = ['repelen*','espiral','mosquiter*']
+
+    for i in range(0,len(tweets)):
+        try:
+            print(str(tweets[i].text))
+        except:
+            pass
+            
 
 capturar_old_tweets()
